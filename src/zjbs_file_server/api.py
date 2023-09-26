43d27a4eb4b37dba8664c70211ba61fddb9e9659
蔡涛 <caitao@zhejianglab.com>
2023-09-26 16:47:07 +0800

    添加UploadZip接口，Delete接口添加recusive参数

--- src/zjbs_file_server/api.py
+++ src/zjbs_file_server/api.py
@@ -1,6 +1,8 @@
+import shutil
 from datetime import datetime
 from pathlib import Path
 from typing import Annotated
+from zipfile import ZipFile
 
 from fastapi import APIRouter, File, Query, UploadFile
 from fastapi.responses import FileResponse
@@ -10,16 +12,16 @@ from zjbs_file_server.error import raise_bad_request, raise_not_found
 from zjbs_file_server.model import FileSystemInfo, FileType, UrlPath
 from zjbs_file_server.settings import settings
 
-router = APIRouter(prefix="/file", tags=["file"])
+router = APIRouter(tags=["file"])
 
 
-@router.post("/upload", description="上传文件")
+@router.post("/Upload", description="上传文件")
 def upload_file(
     directory: Annotated[UrlPath, Query(description="目标文件夹")],
     file: Annotated[UploadFile, File(description="上传的文件")],
     mkdir: Annotated[bool, Query(description="是否创建目录")] = False,
     allow_overwrite: Annotated[bool, Query(description="是否允许覆盖已有文件")] = False,
-) -> bool:
+) -> None:
     destination_folder = get_os_path(directory)
     if not destination_folder.exists():
         if mkdir:
@@ -38,13 +40,31 @@ def upload_file(
             while chunk := file.file.read(settings.BUFFER_SIZE):
                 destination_file.write(chunk)
         logger.info(f"upload_file success: {destination_path}")
-        return True
     except IOError:
         logger.exception(f"upload_file fail: io error: {destination_path}")
         raise
 
 
-@router.post("/download", description="下载文件")
+@router.post("/UploadZip", description="上传并解压缩zip文件")
+def upload_zip(
+    directory: Annotated[UrlPath, Query(description="目标文件夹")],
+    file: Annotated[UploadFile, File(description="上传的文件")],
+    mkdir: Annotated[bool, Query(description="是否创建目录")] = False,
+    allow_overwrite: Annotated[bool, Query(description="是否允许覆盖已有文件")] = False,
+    zip_metadata_encoding: Annotated[str, Query(description="zip文件元数据编码")] = "GB18030",
+) -> None:
+    upload_file(directory, file, mkdir, allow_overwrite)
+
+    destination_dir = get_os_path(directory)
+    zip_path = destination_dir / file.filename
+    with ZipFile(zip_path, mode="r", metadata_encoding=zip_metadata_encoding) as zip_file:
+        zip_file.extractall(destination_dir)
+
+    zip_path.unlink()
+    logger.info(f"upload_zip success: {destination_dir}")
+
+
+@router.post("/Download", description="下载文件")
 def download_file(path: Annotated[UrlPath, Query(description="文件路径")]) -> FileResponse:
     file_path = get_os_path(path)
     if not file_path.exists():
@@ -55,18 +75,34 @@ def download_file(path: Annotated[UrlPath, Query(description="文件路径")]) -
     return FileResponse(file_path, filename=file_path.name)
 
 
-@router.post("/delete", description="删除文件")
-def delete_file(path: Annotated[UrlPath, Query(description="文件路径")]) -> bool:
+@router.post("/Delete", description="删除文件")
+def delete_file(
+    path: Annotated[UrlPath, Query(description="文件路径")], recursive: Annotated[bool, Query(description="是否递归删除")] = False
+) -> bool:
     file_path = get_os_path(path)
-    if file_path.exists():
-        logger.info(f"delete file success: {file_path}")
+    if not file_path.exists():
+        logger.error(f"delete file fail: file not exists: {file_path}")
+        return False
+    if file_path.is_file():
         file_path.unlink()
+        logger.info(f"delete file success: {file_path}")
         return True
-    logger.error(f"delete file fail: file not exists: {file_path}")
-    return False
+    if file_path.is_dir():
+        if recursive:
+            shutil.rmtree(file_path)
+            logger.info(f"delete directory success: {file_path}")
+            return True
+        else:
+            try:
+                file_path.rmdir()
+                logger.info(f"delete empty directory success: {file_path}")
+                return True
+            except OSError:
+                logger.error(f"delete empty directory fail: {file_path}")
+                return False
 
 
-@router.post("/list", description="获取文件列表")
+@router.post("/List", description="获取文件列表")
 def list_directory(directory: Annotated[UrlPath, Query(description="文件路径")]) -> list[FileSystemInfo]:
     file_path = get_os_path(directory)
     if not file_path.exists():
