bd3899caeaf5c66547a293fdd4747074fc212066
蔡涛 <caitao@zhejianglab.com>
2023-11-22 17:45:07 +0800

    写了新的build-image.py，支持自定义registry

--- deploy/build-image.py
+++ deploy/build-image.py
@@ -1,39 +1,53 @@
-import os
+import json
+import re
 import subprocess
+from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser, Namespace
 from datetime import datetime
 from pathlib import Path
 
-CWD: Path = Path(os.environ.get("CWD", Path(__file__).parent.parent)).absolute()
-DOCKER_FILE: Path = Path(os.environ.get("DOCKER_FILE", CWD / "deploy" / "Dockerfile")).absolute()
-DOCKER_USER: str = str(os.environ.get("DOCKER_USER", "cnife"))
-DOCKER_IMAGE_REPO: str = str(os.environ.get("DOCKER_IMAGE_REPO", "zjbs-file-server"))
+cwd = Path(__file__).parent
 
 
-def main() -> None:
-    current_time = datetime.now().strftime("%Y%m%d-%H%M%S")
-    git_hash = subprocess.run(
-        ["git", "-C", str(CWD), "rev-parse", "--short=8", "HEAD"], capture_output=True, text=True, check=True
-    ).stdout.strip()
-    image_version = f"{current_time}-{git_hash}"
-    time_hash_tag = f"{DOCKER_USER}/{DOCKER_IMAGE_REPO}:{image_version}"
-    latest_tag = f"{DOCKER_USER}/{DOCKER_IMAGE_REPO}:latest"
+def new_main(config_path: Path, registry_name: str, push: bool) -> None:
+    config = json.loads(config_path.read_bytes())
+    for registry in config["registries"]:
+        if registry["name"] == registry_name:
+            break
+    else:
+        raise ValueError(f"registry '{registry_name}' not found")
 
-    subprocess.run(
-        [
-            "docker",
-            "build",
-            "--file",
-            str(DOCKER_FILE),
-            "--tag",
-            time_hash_tag,
-            "--tag",
-            latest_tag,
-            "--push",
-            str(CWD),
-        ],
-        check=True,
-    )
+    versions = []
+    for version in config["versions"]:
+        if re.search(r"\{git_hash(:\S+)?}", version):
+            git_hash = subprocess.run(
+                ["git", "-C", str(cwd), "rev-parse", "--short=8", "HEAD"], capture_output=True, text=True, check=True
+            ).stdout.strip()
+            version = version.format(git_hash=git_hash)
+        if "%" in version:
+            version = datetime.now().strftime(version)
+        versions.append(version)
+
+    registry_url = "" if registry["registry"] == "default" else registry["registry"] + "/"
+    user_name = "" if registry["user"] is None else registry["user"] + "/"
+    tags = [f"{registry_url}{user_name}{config['repo']}:{version}" for version in versions]
+
+    docker_cmd = ["docker", "build", "--file", str(cwd / config["dockerfile"])]
+    for tag in tags:
+        docker_cmd.extend(["--tag", tag])
+    if push:
+        docker_cmd.append("--push")
+    docker_cmd.append(str(cwd / config["cwd"]))
+    subprocess.run(docker_cmd, check=True)
+
+
+def parse_args() -> Namespace:
+    parser = ArgumentParser(description="构建镜像", formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument("registry", help="推送到哪个docker registry")
+    parser.add_argument("--config", "-c", type=Path, default=cwd / "build-image-config.json", help="配置文件")
+    parser.add_argument("--push", "-p", action="store_true", default=False, help="是否推送镜像")
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
-    main()
+    args = parse_args()
+    new_main(args.config, args.registry, args.push)
